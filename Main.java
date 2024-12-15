2122e048f831c65cc10802b89f1b952c966e1c18
B VINAY <vinay76b@gmail.com>
2024-12-15 11:21:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+// implementing runnable interface
+class a implements Runnable
+{
+    public void run()
+    {
+        System.out.println("hello i'm thread");
+    }
+}
+public class Main
+{
+    public static void main(String args[])
+    {
+        a r=new a();
+        Thread s=new Thread(r);
+        s.start();
+        s.run();
+    }
+}
+    
